9af757a4154d7a4169e67c6573b0200618c03f72
Mikhail Kulakov <mkulakov@inno.tech>
2023-05-17 02:23:51 +0300

    Реализован метод вычисления операций с двумя числами.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма введённых числе: " + sum);
+
+        int different = firstNumber - secondNumber;
+        System.out.println("Разность введённых чисел: " + different);
+
+        int produce = firstNumber * secondNumber;
+        System.out.println("Произведение введённых чисел: " + produce);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное введённых чисел: " + quotient);
     }
 }
