7c5b9f08dcea0e7d8e01b12edb52a2e160d90541
Mikhail Kulakov <mkulakov@inno.tech>
2023-07-21 16:08:54 +0300

    Реализованы методы вычисления статистики по ОС, а также получения списка страниц из логов

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,19 +1,59 @@
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Statistics {
     private long totalTraffic = 0;
+    private HashSet<String> pageList = new HashSet<>();
+    private HashMap<String, Integer> osList = new HashMap<>();
+    private HashMap<String, Long> osPart = new HashMap<>();
     private LocalDateTime minTime = LocalDateTime.MAX;
     private LocalDateTime maxTime = LocalDateTime.MIN;
-
 
     public void addEntry(LogEntry logEntry) {
         totalTraffic += logEntry.getResponseSize();
         LocalDateTime currentDateTime = logEntry.getTime();
         if (currentDateTime.isBefore(minTime)) minTime = currentDateTime;
         if (currentDateTime.isAfter(maxTime)) maxTime = currentDateTime;
+
+        if (logEntry.getResponseCode() == 200) pageList.add(logEntry.getPath());
+
+        if (logEntry.getUserAgent() != null) {
+            if (!osList.containsKey(logEntry.getUserAgent().getOsType())) {
+                osList.put(logEntry.getUserAgent().getOsType(), 1);
+            } else {
+                osList.put(logEntry.getUserAgent().getOsType(), osList.get(logEntry.getUserAgent().getOsType()) + 1);
+            }
+        }
     }
 
-    public long getTrafficRate() {
-        return (long) totalTraffic / maxTime.getHour() - minTime.getHour();
+    public HashSet<String> getPageList() {
+        return pageList;
     }
+
+    public HashMap<String, Integer> getOsList() {
+        return osList;
+    }
+
+    public double getTrafficRate() {
+        return (double) (totalTraffic / (maxTime.getHour() - minTime.getHour()));
+    }
+
+    public HashMap<String, Double> getOsPart() {
+        return calcOsPart(osList);
+    }
+
+    private HashMap<String, Double> calcOsPart(HashMap<String, Integer> osList) {
+        HashMap<String, Double> osPart = new HashMap<>();
+        int osAllCount = 0;
+        for (int i : osList.values()) {
+            osAllCount += i;
+        }
+        for (String os : osList.keySet()) {
+            osPart.put(os, (double) osList.get(os) / osAllCount);
+        }
+        return osPart;
+    }
+
+
 }
